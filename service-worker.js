41a6423fc88cfc14e939dbb3a64f4d9015866a03
gzr040905-cmd <gzr040905@gmail.com>
2026-06-17 14:35:07 +0800

    Add service worker

---
+++ service-worker.js
@@ -0,0 +1,32 @@
+const CACHE_NAME = "lingxi-paper-horse-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./style.css",
+  "./game.js",
+  "./manifest.webmanifest",
+  "./icon.svg",
+  "./README.md",
+  "./DEPLOY.md",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key)))),
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(
+    caches.match(event.request).then((cached) => cached || fetch(event.request)),
+  );
+});
